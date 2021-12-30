4fe5ff911dc451cc5bd6ed267968d2c8242294ab
Agraj Yadav <agrajy10@gmail.com>
2021-12-30 10:38:02 +0530

    created hero section and changed header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,14 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header className="border-b border-b-gray-200 py-5">
+    <header className="py-5 absolute top-0 left-0 w-full z-40 border-b border-gray-50 border-opacity-10">
       <div className="max-w-7xl mx-auto px-3 flex items-center justify-start">
-        <h1 className="font-extrabold text-3xl text-gray-800">
+        <h1 className="font-extrabold text-3xl text-white hover:opacity-90">
           <Link to="/">Rent or Sell</Link>
         </h1>
-        <Link to="/login" className="btn btn-ghost mr-4 ml-auto px-7">
+        <Link
+          to="/login"
+          className="btn btn-link text-white mr-4 ml-auto px-7 hover:no-underline hover:opacity-60">
           Login
         </Link>
         <Link to="/signup" className="btn btn-primary px-7">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,28 @@
+import { Link } from 'react-router-dom';
+import heroSectionBg from '../assets/images/hero-section-bg.jpg';
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <main>
+      <section className="w-full h-[40rem] md:h-[50rem] relative">
+        <img src={heroSectionBg} alt="" className="w-full h-full object-cover" />
+        <div className="bg-black opacity-40 absolute inset-0"></div>
+        <div className=" w-full p-7 text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white">
+          <h2 className="text-5xl md:text-6xl lg:text-7xl font-semibold leading-none mb-8 max-w-4xl mx-auto">
+            Rent or sell your house at <span className="font-black">best price</span>.
+          </h2>
+          <Link
+            to="/login"
+            className="btn bg-white text-neutral normal-case hover:bg-gray-200 hover:text-neutral mx-2 mb-4 w-48">
+            List your house
+          </Link>
+          <button type="button" className="btn btn-primary normal-case mx-2 mb-4 w-48">
+            Explore
+          </button>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default Home;
