5e1436db1d497bea182a691e21cb2357861c8d3d
Agraj Yadav <agrajy10@gmail.com>
2022-01-12 17:43:29 +0530

    Added filter by type in saved listings page

--- src/pages/SavedListings.jsx
+++ src/pages/SavedListings.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useContext, useRef } from 'react';
 import { doc, getDoc } from 'firebase/firestore';
 
 import ListingItem from '../components/ListingItem';
@@ -8,9 +8,12 @@ import { FavoritesContext } from '../context/FavoritesContext';
 import { db } from '../firebase.config';
 
 function SavedListings() {
+  const initalRender = useRef(true);
   const [loading, setLoading] = useState(true);
   const [listings, setListings] = useState([]);
+  const [filteredListings, setFilteredListings] = useState([]);
   const [error, setError] = useState('');
+  const [listingTypeOption, setListingTypeOption] = useState('all');
 
   const { favorites, checkFavorite } = useContext(FavoritesContext);
 
@@ -25,11 +28,25 @@ function SavedListings() {
       ).catch((error) => setError(error.message));
       const savedListings = savedListingDocs.map((doc) => ({ docID: doc.id, data: doc.data() }));
       setListings(savedListings);
+      setFilteredListings(savedListings);
       setLoading(false);
     };
 
     getSavedListings();
   }, [favorites]);
+
+  useEffect(() => {
+    if (!initalRender.current) {
+      if (listingTypeOption === 'all') {
+        setFilteredListings(listings);
+      } else {
+        const filterResults = listings.filter((listing) => listing.data.type === listingTypeOption);
+        setFilteredListings(filterResults);
+      }
+    } else {
+      initalRender.current = false;
+    }
+  }, [listingTypeOption]);
 
   if (loading) {
     return (
@@ -52,10 +69,18 @@ function SavedListings() {
       <section className="lg:py-24 md:py-20 py-14">
         <div className="md:flex md:items-center md:justify-between">
           <h1 className="text-3xl md:text-4xl font-extrabold text-center mb-8">Saved Listings</h1>
+          <select
+            className="select select-bordered w-full max-w-xs mb-8 mx-auto md:mx-0 block"
+            value={listingTypeOption}
+            onChange={(e) => setListingTypeOption(e.target.value)}>
+            <option value="all">All</option>
+            <option value="sale">For Sale</option>
+            <option value="rent">For Rent</option>
+          </select>
         </div>
         <div className="grid grid-cols-1 gap-4 xl:gap-8 sm:grid-cols-2 lg:grid-cols-3">
-          {listings.length ? (
-            listings.map(({ docID, data }) => (
+          {filteredListings.length ? (
+            filteredListings.map(({ docID, data }) => (
               <ListingItem {...data} key={docID} docID={docID} isFavorite={checkFavorite(docID)} />
             ))
           ) : (
