c1e831a509ca33979532766967b8551fd2449ac9
Agraj Yadav <agrajy10@gmail.com>
2022-01-16 16:54:53 +0530

    Reduced font size of Listing info card

--- src/pages/listing-details/ListingPrice.jsx
+++ src/pages/listing-details/ListingPrice.jsx
@@ -2,7 +2,7 @@ import { formatPrice } from '../../utils/utils';
 
 function ListingPrice({ regularPrice, type }) {
   return (
-    <p className="font-bold text-gray-900 text-[2.5rem] pb-6 mb-6 border-b border-b-gray-300">
+    <p className="font-bold text-gray-900 text-[2.2rem] md:text-[2.5rem] pb-6 mb-6 border-b border-b-gray-300">
       {formatPrice(regularPrice)}
       {type === 'rent' ? '/month' : null}
     </p>
